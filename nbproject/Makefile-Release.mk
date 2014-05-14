db910842a5f9b2c4566137cc4d29051fa78c5e8b
Aaron Hebert <aaron.hebert@gmail.com>
2014-05-14 09:39:16 -0700

    fixed broken travis build; moved source files to src

--- nbproject/Makefile-Release.mk
+++ nbproject/Makefile-Release.mk
@@ -36,23 +36,23 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 # Object Files
 OBJECTFILES= \
 	${OBJECTDIR}/api/bgp_api.o \
-	${OBJECTDIR}/bgp.o \
-	${OBJECTDIR}/bgp_attrib_alias.o \
-	${OBJECTDIR}/bgp_database.o \
-	${OBJECTDIR}/bgp_main.o \
-	${OBJECTDIR}/bgp_notification.o \
-	${OBJECTDIR}/bgp_open.o \
-	${OBJECTDIR}/bgp_peer.o \
-	${OBJECTDIR}/bgp_send.o \
-	${OBJECTDIR}/bgp_syslog.o \
-	${OBJECTDIR}/bgp_thread-mgr.o \
-	${OBJECTDIR}/bgp_timer.o \
-	${OBJECTDIR}/bgp_update.o \
 	${OBJECTDIR}/cli/bgp_cli.o \
 	${OBJECTDIR}/cli/bgp_cmdparser.o \
-	${OBJECTDIR}/misc.o \
-	${OBJECTDIR}/tcp_client.o \
-	${OBJECTDIR}/tcp_server.o
+	${OBJECTDIR}/src/bgp.o \
+	${OBJECTDIR}/src/bgp_attrib_alias.o \
+	${OBJECTDIR}/src/bgp_database.o \
+	${OBJECTDIR}/src/bgp_main.o \
+	${OBJECTDIR}/src/bgp_notification.o \
+	${OBJECTDIR}/src/bgp_open.o \
+	${OBJECTDIR}/src/bgp_peer.o \
+	${OBJECTDIR}/src/bgp_send.o \
+	${OBJECTDIR}/src/bgp_syslog.o \
+	${OBJECTDIR}/src/bgp_thread-mgr.o \
+	${OBJECTDIR}/src/bgp_timer.o \
+	${OBJECTDIR}/src/bgp_update.o \
+	${OBJECTDIR}/src/misc.o \
+	${OBJECTDIR}/src/tcp_client.o \
+	${OBJECTDIR}/src/tcp_server.o
 
 
 # C Compiler Flags
@@ -84,66 +84,6 @@ ${OBJECTDIR}/api/bgp_api.o: api/bgp_api.cpp
 	${RM} "$@.d"
 	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/api/bgp_api.o api/bgp_api.cpp
 
-${OBJECTDIR}/bgp.o: bgp.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/bgp.o bgp.cpp
-
-${OBJECTDIR}/bgp_attrib_alias.o: bgp_attrib_alias.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/bgp_attrib_alias.o bgp_attrib_alias.cpp
-
-${OBJECTDIR}/bgp_database.o: bgp_database.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/bgp_database.o bgp_database.cpp
-
-${OBJECTDIR}/bgp_main.o: bgp_main.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/bgp_main.o bgp_main.cpp
-
-${OBJECTDIR}/bgp_notification.o: bgp_notification.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/bgp_notification.o bgp_notification.cpp
-
-${OBJECTDIR}/bgp_open.o: bgp_open.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/bgp_open.o bgp_open.cpp
-
-${OBJECTDIR}/bgp_peer.o: bgp_peer.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/bgp_peer.o bgp_peer.cpp
-
-${OBJECTDIR}/bgp_send.o: bgp_send.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/bgp_send.o bgp_send.cpp
-
-${OBJECTDIR}/bgp_syslog.o: bgp_syslog.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/bgp_syslog.o bgp_syslog.cpp
-
-${OBJECTDIR}/bgp_thread-mgr.o: bgp_thread-mgr.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/bgp_thread-mgr.o bgp_thread-mgr.cpp
-
-${OBJECTDIR}/bgp_timer.o: bgp_timer.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/bgp_timer.o bgp_timer.cpp
-
-${OBJECTDIR}/bgp_update.o: bgp_update.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/bgp_update.o bgp_update.cpp
-
 ${OBJECTDIR}/cli/bgp_cli.o: cli/bgp_cli.cpp 
 	${MKDIR} -p ${OBJECTDIR}/cli
 	${RM} "$@.d"
@@ -154,20 +94,80 @@ ${OBJECTDIR}/cli/bgp_cmdparser.o: cli/bgp_cmdparser.cpp
 	${RM} "$@.d"
 	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/cli/bgp_cmdparser.o cli/bgp_cmdparser.cpp
 
-${OBJECTDIR}/misc.o: misc.cpp 
-	${MKDIR} -p ${OBJECTDIR}
+${OBJECTDIR}/src/bgp.o: src/bgp.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/misc.o misc.cpp
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/bgp.o src/bgp.cpp
 
-${OBJECTDIR}/tcp_client.o: tcp_client.cpp 
-	${MKDIR} -p ${OBJECTDIR}
+${OBJECTDIR}/src/bgp_attrib_alias.o: src/bgp_attrib_alias.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/tcp_client.o tcp_client.cpp
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/bgp_attrib_alias.o src/bgp_attrib_alias.cpp
 
-${OBJECTDIR}/tcp_server.o: tcp_server.cpp 
-	${MKDIR} -p ${OBJECTDIR}
+${OBJECTDIR}/src/bgp_database.o: src/bgp_database.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/tcp_server.o tcp_server.cpp
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/bgp_database.o src/bgp_database.cpp
+
+${OBJECTDIR}/src/bgp_main.o: src/bgp_main.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/bgp_main.o src/bgp_main.cpp
+
+${OBJECTDIR}/src/bgp_notification.o: src/bgp_notification.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/bgp_notification.o src/bgp_notification.cpp
+
+${OBJECTDIR}/src/bgp_open.o: src/bgp_open.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/bgp_open.o src/bgp_open.cpp
+
+${OBJECTDIR}/src/bgp_peer.o: src/bgp_peer.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/bgp_peer.o src/bgp_peer.cpp
+
+${OBJECTDIR}/src/bgp_send.o: src/bgp_send.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/bgp_send.o src/bgp_send.cpp
+
+${OBJECTDIR}/src/bgp_syslog.o: src/bgp_syslog.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/bgp_syslog.o src/bgp_syslog.cpp
+
+${OBJECTDIR}/src/bgp_thread-mgr.o: src/bgp_thread-mgr.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/bgp_thread-mgr.o src/bgp_thread-mgr.cpp
+
+${OBJECTDIR}/src/bgp_timer.o: src/bgp_timer.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/bgp_timer.o src/bgp_timer.cpp
+
+${OBJECTDIR}/src/bgp_update.o: src/bgp_update.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/bgp_update.o src/bgp_update.cpp
+
+${OBJECTDIR}/src/misc.o: src/misc.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/misc.o src/misc.cpp
+
+${OBJECTDIR}/src/tcp_client.o: src/tcp_client.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/tcp_client.o src/tcp_client.cpp
+
+${OBJECTDIR}/src/tcp_server.o: src/tcp_server.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/tcp_server.o src/tcp_server.cpp
 
 # Subprojects
 .build-subprojects:
